5c3e648096662107809b0b002ea5bb06505f63e3
Your Name <you@example.com>
2023-03-17 18:02:47 +0100

    quasi finita la seconda milestone

--- milestone-due/milestone-due/main.js
+++ milestone-due/milestone-due/main.js
@@ -2,72 +2,193 @@
 
 const { createApp } = Vue;
 
-const app = Vue.createApp({
+const app = createApp({
     data() {
         return {
-            contatti: [
+            contacts: [
                 {
-                    nome: 'Tizio Caio',
-                    immagine: 'img/avatar_3.jpg',
-                    ultimoAccesso: '6.66',
+                    name: "Sofia",
+                    avatar: "img/avatar_1.jpg",
+                    lastAccess: "6.66",
+                    lastMessage: {
+                        date: "10/03/2023",
+                    },
+                    messages: [
+                        {
+                            text: "Ciao Sofia!",
+                            date: "10/03/2023",
+                        },
+                        {
+                            text: "Come stai?",
+                            date: "10/03/2023",
+                        },
+                    ],
                 },
                 {
-                    nome: 'Giggio Basdiggio',
-                    immagine: 'img/avatar_4.jpg',
-                    ultimoAccesso: '6.66',
+                    name: "Tizio Caio",
+                    avatar: "img/avatar_3.jpg",
+                    lastAccess: "6.66",
+                    lastMessage: {
+                        date: "10/03/2023",
+                    },
+                    messages: [
+                        {
+                            text: "Ciao Tizio!",
+                            date: "10/03/2023",
+                        },
+                        {
+                            text: "Tutto bene grazie, tu?",
+                            date: "10/03/2023",
+                        },
+                    ],
                 },
                 {
-                    nome: 'Fabio Giorgini',
-                    immagine: 'img/avatar_3.jpg',
-                    ultimoAccesso: '6.66',
+                    name: "Giggio Basdiggio",
+                    avatar: "img/avatar_4.jpg",
+                    lastAccess: "6.66",
+                    lastMessage: {
+                        date: "10/03/2023",
+                    },
+                    messages: [
+                        {
+                            text: "Ciao Giggio!",
+                            date: "10/03/2023",
+                        },
+                        {
+                            text: "Come va?",
+                            date: "10/03/2023",
+                        },
+                    ],
                 },
                 {
-                    nome: 'Cristian Cosenza',
-                    immagine: 'img/avatar_4.jpg',
-                    ultimoAccesso: '6.66',
+                    name: "Cristian Cosenza",
+                    avatar: "img/avatar_4.jpg",
+                    lastAccess: "6.66",
+                    lastMessage: {
+                        date: "10/03/2023",
+                    },
+                    messages: [
+                        {
+                            text: "Ciao,mi aiuti co gli esercizi?",
+                            date: "10/03/2023",
+                        },
+                        {
+                            text: "si",
+                            date: "10/03/2023",
+                        },
+                    ],
                 },
                 {
-                    nome: 'Gaetano frascolla',
-                    immagine: 'img/avatar_3.jpg',
-                    ultimoAccesso: '6.66',
+                    name: "La pasta Amatriciana",
+                    avatar: "img/avatar_4.jpg",
+                    lastAccess: "6.66",
+                    lastMessage: {
+                        date: "10/03/2023",
+                    },
+                    messages: [
+                        {
+                            text: "Sei molto buona!",
+                            date: "10/03/2023",
+                        },
+                        {
+                            text: "Lo so",
+                            date: "10/03/2023",
+                        },
+                    ],
                 },
                 {
-                    nome: 'La pasta Amatriciana',
-                    immagine: 'img/avatar_4.jpg',
-                    ultimoAccesso: '6.66',
+                    name: "Gaetano frascolla",
+                    avatar: "img/avatar_4.jpg",
+                    lastAccess: "6.66",
+                    lastMessage: {
+                        date: "10/03/2023",
+                    },
+                    messages: [
+                        {
+                            text: "Ciao,mi aiuti co gli esercizi?",
+                            date: "10/03/2023",
+                        },
+                        {
+                            text: "Si",
+                            date: "10/03/2023",
+                        },
+                    ],
                 },
                 {
-                    nome: 'è migliore',
-                    immagine: 'img/avatar_3.jpg',
-                    ultimoAccesso: '6.66',
+                    name: "Fabio Giorgini",
+                    avatar: "img/avatar_1.jpg",
+                    lastAccess: "6.66",
+                    lastMessage: {
+                        date: "10/03/2023",
+                    },
+                    messages: [
+                        {
+                            text: "Ciao, mi aiuti con gli esercizi?",
+                            date: "10/03/2023",
+                        },
+                        {
+                            text: "Si",
+                            date: "10/03/2023",
+                        },
+                    ],
                 },
                 {
-                    nome: 'Della Carbonara',
-                    immagine: 'img/avatar_4.jpg',
-                    ultimoAccesso: '6.66',
+                    name: "Fabio Giorgini",
+                    avatar: "img/avatar_1.jpg",
+                    lastAccess: "6.66",
+                    lastMessage: {
+                        date: "10/03/2023",
+                    },
+                    messages: [
+                        {
+                            text: "Ciao, mi aiuti con gli esercizi?",
+                            date: "10/03/2023",
+                        },
+                        {
+                            text: "Si",
+                            date: "10/03/2023",
+                        },
+                    ],
                 },
                 {
-                    nome: 'Tizio Caio',
-                    immagine: 'img/avatar_3.jpg',
-                    ultimoAccesso: '6.66',
+                    name: "Fabio Giorgini",
+                    avatar: "img/avatar_1.jpg",
+                    lastAccess: "6.66",
+                    lastMessage: {
+                        date: "10/03/2023",
+                    },
+                    messages: [
+                        {
+                            text: "Ciao, mi aiuti con gli esercizi?",
+                            date: "10/03/2023",
+                        },
+                        {
+                            text: "Si",
+                            date: "10/03/2023",
+                        },
+                    ],
                 },
-                {
-                    nome: 'Giggio Basdiggio',
-                    immagine: 'img/avatar_4.jpg',
-                    ultimoAccesso: '6.66',
-                },
-                {
-                    nome: 'Tizio Caio',
-                    immagine: 'img/avatar_3.jpg',
-                    ultimoAccesso: '6.66',
-                },
-                {
-                    nome: 'Giggio Basdiggio',
-                    immagine: 'img/avatar_4.jpg',
-                    ultimoAccesso: '6.66',
-                },
-
-            ]
-        }
-    }
-}).mount("#app");
+            ],
+            activeContact: {
+                name: "Fabio Giorgini",
+                lastAccess: "6.66",
+                messages: [
+                    {
+                        text: "Ciao,mi aiuti con gli esercizi?",
+                        date: "10/03/2023",
+                    },
+                    {
+                        text: "si",
+                        date: "10/03/2023",
+                    },
+                ],
+            },
+        };
+    },
+    methods: {
+        setActiveContact(contact) {
+            this.activeContact = contact;
+        },
+    },
+});
+app.mount("#app");
