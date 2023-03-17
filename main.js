a72e2425c6a72f1a4da2e772d1cb708f3c93dd62
Your Name <you@example.com>
2023-03-17 16:16:12 +0100

    milestone 1

--- main.js
+++ main.js
@@ -1,1 +1,73 @@
 "use strict";
+
+const { createApp } = Vue;
+
+const app = Vue.createApp({
+    data() {
+        return {
+            contatti: [
+                {
+                    nome: 'Tizio Caio',
+                    immagine: 'img/avatar_3.jpg',
+                    ultimoAccesso: '6.66',
+                },
+                {
+                    nome: 'Giggio Basdiggio',
+                    immagine: 'img/avatar_4.jpg',
+                    ultimoAccesso: '6.66',
+                },
+                {
+                    nome: 'Fabio Giorgini',
+                    immagine: 'img/avatar_3.jpg',
+                    ultimoAccesso: '6.66',
+                },
+                {
+                    nome: 'Cristian Cosenza',
+                    immagine: 'img/avatar_4.jpg',
+                    ultimoAccesso: '6.66',
+                },
+                {
+                    nome: 'Gaetano frascolla',
+                    immagine: 'img/avatar_3.jpg',
+                    ultimoAccesso: '6.66',
+                },
+                {
+                    nome: 'La pasta Amatriciana',
+                    immagine: 'img/avatar_4.jpg',
+                    ultimoAccesso: '6.66',
+                },
+                {
+                    nome: 'è migliore',
+                    immagine: 'img/avatar_3.jpg',
+                    ultimoAccesso: '6.66',
+                },
+                {
+                    nome: 'Della Carbonara',
+                    immagine: 'img/avatar_4.jpg',
+                    ultimoAccesso: '6.66',
+                },
+                {
+                    nome: 'Tizio Caio',
+                    immagine: 'img/avatar_3.jpg',
+                    ultimoAccesso: '6.66',
+                },
+                {
+                    nome: 'Giggio Basdiggio',
+                    immagine: 'img/avatar_4.jpg',
+                    ultimoAccesso: '6.66',
+                },
+                {
+                    nome: 'Tizio Caio',
+                    immagine: 'img/avatar_3.jpg',
+                    ultimoAccesso: '6.66',
+                },
+                {
+                    nome: 'Giggio Basdiggio',
+                    immagine: 'img/avatar_4.jpg',
+                    ultimoAccesso: '6.66',
+                },
+
+            ]
+        }
+    }
+}).mount("#app");
